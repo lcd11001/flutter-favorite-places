0a5a10fa7b4c84fc8e968bc1e13eec80d692e71e
GAMELOFT\dan.luongcong <dan.luongcong@gameloft.com>
2024-04-24 15:26:57 +0700

    + custom circle cliper

---
+++ lib/utils/circle_reveal_cliper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CircleRevealCliper extends CustomClipper<Rect> {
+  final double fraction;
+
+  CircleRevealCliper(this.fraction);
+
+  @override
+  Rect getClip(Size size) {
+    final radius = size.width * fraction;
+    final diameter = 2 * radius;
+    final center = Offset(size.width / 2, size.height / 2);
+    final topLeft = center - Offset(radius, radius);
+    final bottomRight = center + Offset(radius, radius);
+    return Rect.fromPoints(topLeft, bottomRight);
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+}
