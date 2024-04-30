ac8d9942f9d79dd1e4b83230df411014de90822a
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-05-01 06:07:12 +0700

    - detail place

--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:favorite_places/api/google_maps_api.dart';
 import 'package:favorite_places/widgets/place_portrait.dart';
 import 'package:flutter/material.dart';
 
@@ -18,27 +20,50 @@ class PlaceDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(place.title),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              PlacePortrait(place: place),
-              const SizedBox(height: 16),
-              PlaceText(
-                content: place.title,
-                size: PlaceTextSize.large,
-              ),
-              const SizedBox(height: 32),
-              PlaceText(
-                content: place.address,
-                size: PlaceTextSize.small,
-                alignment: TextAlign.justify,
-              ),
-              const SizedBox(height: 64),
-            ],
+      body: Stack(
+        children: [
+          PlacePortrait(
+            place: place,
+            height: double.infinity,
           ),
-        ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: CachedNetworkImageProvider(
+                    GoogleMapsApi.getStaticMapImageUrl(
+                      place.latitude,
+                      place.longitude,
+                      zoom: 16,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                    colors: [
+                      Colors.transparent,
+                      Colors.black87,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  )),
+                  child: PlaceText(
+                    content: place.address,
+                    size: PlaceTextSize.small,
+                    alignment: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
